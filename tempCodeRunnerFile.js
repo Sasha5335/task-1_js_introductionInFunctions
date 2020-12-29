e22439e2b175d486bd570fc4ac3992bcd8e10032
Aleksandr <aleksandr@gmail.com>
2020-12-29 09:05:40 +0200

    Work on bugs

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,35 +1,4 @@
 
-/**
- * Проверять пользователя на совершенолетие
- * @param {number} age возраст пользователя
- * @returns {boolean || null} true если пользователю 18 и больше, false если меньше 
- */
-//описание себе для примера
-
-const isAdult = function (age) {
-
-	if (isNaN(age)) return null;
-	return age >= 18;
-
-};
-
-console.log(isAdult(18));
-
-// ---------------------------------------------------------------------------------------------
-
-const checkMultiplicity = function (num1, num2) {
-
-	if (num1 % num2 === 0) return true;
-	return false;
-};
-
-console.log(checkMultiplicity(25, 5)); // true
-console.log(checkMultiplicity(15, 3)); // true
-console.log(checkMultiplicity(15, 5)); // true
-console.log(checkMultiplicity(15, 4)); // false
-
-// ---------------------------------------------------------------------------------------------
-
 const triangleTest = function (sideA, sideB, sideC) {
 
 	if (sideA + sideB > sideC && sideA + sideC > sideB && sideB + sideC > sideA) return true;
@@ -72,4 +41,4 @@ const areaCalculation = function (a, b) {
 	return [parseFloat(rhombus.toFixed(3)), parseFloat(cylinder.toFixed(3)), parseFloat(rectangle.toFixed(3)), parseFloat(triangle.toFixed(3))];
 };
 
-console.log(areaCalculation(2, 5));
+areaCalculation(2, 5);
